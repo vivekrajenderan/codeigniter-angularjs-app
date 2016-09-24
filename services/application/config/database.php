f0e5c95d3dcfd4c33652f32972d3235957bdca3e
Vivek <vivekinnoppl@gmail.com>
2016-09-24 09:42:51 +0530

    Update database

--- services/application/config/database.php
+++ services/application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'angular';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'angularjs_app';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
